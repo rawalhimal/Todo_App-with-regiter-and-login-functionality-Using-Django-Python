1bf5003d75d701fb1a347d440e615479e6a78a3b
himal <himalrawal500@gmail.com>
2020-09-23 13:08:21 +0545

    Todo app with register and login page

--- Todo_App/App/urls.py
+++ Todo_App/App/urls.py
@@ -4,4 +4,7 @@ urlpatterns=[
     path('',views.index,name='index'),
     path('add_todo/',views.add_todo,name='add_todo'),
     path('delete_todo/<int:todo_id>/', views.delete_todo,name='delete_todo'),
+    path('register/',views.register,name='register'),
+    path('login/',views.loginPage,name='login'),
+    path('logout/',views.logoutUser,name='logout'),
 ]
--- Todo_App/App/views.py
+++ Todo_App/App/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.utils import timezone
 from .models import Todo
 from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='login')
 def index(request):
     todo_items = Todo.objects.all().order_by("-added_date")
     return render(request, 'main/index.html', {
@@ -17,5 +23,41 @@ def add_todo(request):
 def delete_todo(request, todo_id):
   Todo.objects.get(id=todo_id).delete()
   return HttpResponseRedirect("/")
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('')
+    else:
+        if request.method == 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('index')
+            else:
+                messages.info(request,'username or password is incorrect')
+        context={}
+        return render(request,'main/login.html',context)
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+    
+        form=CreateUserForm()
+        if request.method == 'POST':
+            form =CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request,'form was created for'  +  user)
+                return redirect('login')
+        context={'form':form}
+    
+        return render(request,'main/register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
